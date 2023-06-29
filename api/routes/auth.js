dc5555831809fe03dcc2664a01e7ef505c4b6a7d
greenflagss <greenflags07@gmail.com>
2023-06-29 14:47:08 +0530

    Initial Commit

---
+++ api/routes/auth.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    const { password, ...others } = savedUser._doc;
+    res.status(201).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+
+    console.log(user);
+
+    !user && res.status(401).json("Wrong User Name");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    console.log(`Hashed Pass:${hashedPassword}`);
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    console.log(`Original Pass:${originalPassword}`);
+
+    const inputPassword = req.body.password;
+
+    originalPassword !== inputPassword &&
+      res.status(401).json("Wrong Password");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
